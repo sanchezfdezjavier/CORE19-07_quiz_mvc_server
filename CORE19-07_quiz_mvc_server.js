c2715f4a9d152fbf7d1ac0b86c337a44759eab34
practicas-ging <practicas-ging@mail.com>
2019-03-27 12:45:06 +0100

    Updated 07_quiz_mvc_server

---
+++ CORE19-07_quiz_mvc_server.js
@@ -0,0 +1,256 @@
+const express = require('express');
+const app = express();
+
+// Import MW for parsing POST params in BODY
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Import MW supporting Method Override with express
+const methodOverride = require('method-override');
+app.use(methodOverride('_method', {methods: ["POST", "GET"]}));
+
+// ========== MODEL ==========
+
+const Sequelize = require('sequelize');
+
+const options = {logging: false, operatorsAliases: false};
+const sequelize = new Sequelize("sqlite:db.sqlite", options);
+
+const Quiz = sequelize.define(  // define Quiz model (table quizzes)
+    'quiz',
+    {
+        question: Sequelize.STRING,
+        answer: Sequelize.STRING
+    }
+);
+
+sequelize.sync() // Syncronize DB and seed if needed
+    .then(() => Quiz.count())
+    .then(count => {
+        if (count === 0) {
+            return Quiz.bulkCreate([
+                {question: "Capital of Italy", answer: "Rome"},
+                {question: "Capital of France", answer: "Paris"},
+                {question: "Capital of Spain", answer: "Madrid"},
+                {question: "Capital of Portugal", answer: "Lisbon"}
+            ])
+                .then(c => console.log(`DB filled with ${c.length} quizzes.`));
+        } else {
+            console.log(`DB exists & has ${count} quizzes.`);
+        }
+    })
+    .catch(console.log);
+
+
+// ========== VIEWs ==========
+
+// CSS style to include into the views:
+const style = `
+        <style>
+            .button { display: inline-block; text-decoration: none;
+                padding: 2px 6px; margin: 2px;
+                background: #4479BA; color: #FFF;
+                border-radius: 4px; border: solid 1px #20538D; }
+            .button:hover { background: #356094; }
+        </style>`;
+
+// View to display all the quizzes passed into the quizzes parameter.
+const indexView = quizzes =>
+    `<!doctype html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>P7: Quiz</title>
+        ${style}
+    </head>
+    <body>
+        <h1>Quizzes</h1>` +
+    quizzes.map(quiz =>
+        `<div>
+                <a href="/quizzes/${quiz.id}/play">${quiz.question}</a>
+                <a href="/quizzes/${quiz.id}/edit"
+                   class="button">Edit</a>
+                <a href="/quizzes/${quiz.id}?_method=DELETE"
+                   onClick="return confirm('Delete: ${quiz.question}')"
+                   class="button">Delete</a>
+             </div>`).join("\n") +
+    `<a href="/quizzes/new" class="button">New Quiz</a>
+    </body>
+    </html>`;
+
+
+// View to play to the quiz passed into the quiz parameter.
+// response is the response typed in by the user.
+const playView = (quiz, response) =>
+    `<!doctype html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>P7: Quiz</title>
+        ${style}
+    </head>
+    <body>
+        <h1>Play Quiz</h1>
+        <form method="get" action="/quizzes/${quiz.id}/check">
+            ${quiz.question}: <br />
+            <input type="text" name="response" value="${response}"
+placeholder="Answer" />
+            <input type="submit" class="button" value="Check" /> <br />
+        </form>
+        <a href="/quizzes" class="button">Go back</a>
+    </body>
+    </html>`;
+
+
+// View to show the result of playing a quiz.
+// id is the id of the played quiz.
+// msg is the obtained result.
+// response is the answer typed in by the user.
+const resultView = (id, msg, response) =>
+    `<!doctype html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>P7: Quiz</title>
+         ${style}
+   </head>
+    <body>
+        <h1>Result</h1>
+        <div id="msg"><strong>${msg}</strong></div>
+        <a href="/quizzes" class="button">Go back</a>
+        <a href="/quizzes/${id}/play?response=${response}" class="button">Try again</a>
+    </body>
+    </html>`;
+
+
+// View to show a form to create a new quiz.
+const newView = (quiz) => {
+    return `<!doctype html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>P7: Quiz</title>
+        ${style}
+    </head>
+    <body>
+        <h1>Create New Quiz</h1>
+        <form method="POST" action="/quizzes">
+            Question: <input type="text" name="question" value="${quiz.question}"
+placeholder="Question" /> <br />
+            Answer: <input type="text" name="answer"   value="${quiz.answer}"  
+placeholder="Answer" />
+            <input type="submit" class="button" value="Create" /> <br />
+        </form>
+        <a href="/quizzes" class="button">Go back</a>
+    </body>
+    </html>`;
+}
+
+
+// View to show a form to edit the given quiz.
+const editView = (quiz) => {
+    // .... introducir código
+}
+
+
+// ========== CONTROLLERs ==========
+
+// GET /, GET /quizzes
+const indexController = (req, res, next) => {
+    Quiz.findAll()
+        .then(quizzes => res.send(indexView(quizzes)))
+        .catch(next);
+};
+
+//  GET  /quizzes/:id/play
+const playController = (req, res, next) => {
+    const id = Number(req.params.id);
+    if (Number.isNaN(id)) return next(`id "${req.params.id}" is not a number.`);
+
+    const response = req.query.response || "";
+
+    Quiz.findByPk(id)   // Sequelize v5 utiliza findByPk en vez de findById (esta deprecado)
+        .then(quiz => quiz ?
+            res.send(playView(quiz, response)) :
+            next(new Error(`Quiz ${id} not found.`)))
+        .catch(next);
+};
+
+
+//  GET  /quizzes/:id/check
+const checkController = (req, res, next) => {
+    const response = req.query.response;
+
+    const id = Number(req.params.id);
+    if (Number.isNaN(id)) return next(`id "${req.params.id}" is not a number.`);
+
+    Quiz.findByPk(id)   // Sequelize v5 utiliza findByPk en vez de findById (esta deprecado)
+        .then(quiz => {
+            const msg = (quiz.answer.toLowerCase().trim() ===
+                response.toLowerCase().trim()) ?
+                `Yes, "${response}" is the ${quiz.question}`
+                : `No, "${response}" is not the ${quiz.question}`;
+            return res.send(resultView(id, msg, response));
+        })
+        .catch(next);
+};
+
+
+// GET /quizzes/new
+const newController = (req, res, next) => {
+    const quiz = {question: "", answer: ""};
+    res.send(newView(quiz));
+};
+
+// POST /quizzes
+const createController = (req, res, next) => {
+    let {question, answer} = req.body;
+
+    Quiz.create({question, answer})
+        .then(quiz => res.redirect('/quizzes'))
+        .catch(next);
+};
+
+//  GET /quizzes/:id/edit
+const editController = (req, res, next) => {
+    // .... introducir código
+};
+
+//  PUT /quizzes/:id
+const updateController = (req, res, next) => {
+    // .... introducir código
+};
+
+// DELETE /quizzes/:id
+const destroyController = (req, res, next) => {
+    // .... introducir código
+};
+
+
+// ========== ROUTES ==========
+
+app.get(['/', '/quizzes'], indexController);
+app.get('/quizzes/:id/play', playController);
+app.get('/quizzes/:id/check', checkController);
+app.get('/quizzes/new', newController);
+app.post('/quizzes', createController);
+
+// ..... crear rutas e instalar los MWs para:
+//   GET  /quizzes/:id/edit
+//   PUT  /quizzes/:id
+//   DELETE  /quizzes/:id
+
+
+app.all('*', (req, res) =>
+    res.status(404).send("Error: resource not found or method not supported.")
+);
+
+
+// Middleware to manage errors:
+app.use((error, req, res, next) => {
+    console.log("Error:", error.message || error);
+    res.redirect("/");
+});
+
+// Server started at port 8000
+app.listen(8000);
---
+++ tests/checks.test.js
@@ -0,0 +1,205 @@
+/**
+ * Corrector para la práctica de sql
+ */
+
+// IMPORTS
+const should = require('chai').should();
+const path = require('path');
+const fs = require('fs-extra');
+const Utils = require('./utils');
+const to = require('./to');
+const Browser = require('zombie');
+const spawn = require("child_process").spawn;
+
+// CRITICAL ERRORS
+let error_critical = null;
+
+// CONSTANTS
+const T_WAIT = 2; // Time between commands
+const T_TEST = 2 * 60; // Time between tests (seconds)
+const browser = new Browser();
+const URL = "http://localhost:8000/quizzes";
+const path_assignment = path.resolve(path.join(__dirname, "../"));
+const path_file = path.join(path_assignment, "CORE19-07_quiz_mvc_server.js");
+const quizzes_orig = path.join(path_assignment, 'quizzes.sqlite');
+const quizzes_back = path.join(path_assignment, 'quizzes.original.sqlite');
+const quizzes_test = path.join(path_assignment, 'tests', 'quizzes.sqlite');
+
+// HELPERS
+const timeout = ms => new Promise(res => setTimeout(res, ms));
+let server = null;
+
+//TESTS
+describe("CORE19-07_quiz_mvc_server", function () {
+
+    this.timeout(T_TEST * 1000);
+
+    it('', async function () {
+        this.name = `1(Precheck): Checking that the assignment file exists...`;
+        this.score = 0;
+        this.msg_ok = `Found the file '${path_file}'`;
+        this.msg_err = `Couldn't find the file '${path_file}'`;
+        const [error_path, path_ok] = await to(fs.pathExists(path_file));
+        if (error_path) {
+            error_critical = this.msg_err;
+        }
+        path_ok.should.be.equal(true);
+    });
+
+    it('', async function () {
+        this.name = `2(Precheck): Installing dependencies...`;
+        this.score = 0;
+        if (error_critical) {
+            this.msg_err = error_critical;
+            should.not.exist(error_critical);
+        } else {
+            this.msg_ok = "Dependencies installed successfully";
+            // replace answers file
+            let error_deps;
+            try {fs.copySync(quizzes_orig, quizzes_back, {"overwrite": true});} catch (e){}
+            try {fs.copySync(quizzes_test, quizzes_orig, {"overwrite": true});} catch (e){}
+            if (error_deps) {
+                this.msg_err = "Error copying the answers file: " + error_deps;
+                error_critical = this.msg_err;
+            }
+            should.not.exist(error_critical);
+        }
+    });
+
+    it('', async function () {
+        this.name = `3(Precheck): Launching the server...`;
+        this.score = 0;
+        if (error_critical) {
+            this.msg_err = error_critical;
+            should.not.exist(error_critical);
+        } else {
+            this.msg_ok = `'${path_file}' has been launched correctly`;
+            server = spawn("node", [path_file], {cwd: path_assignment});
+            let error_launch = "";
+            server.on('error', function (data) {
+                error_launch += data
+            });
+            server.stderr.on('data', function (data) {
+                error_launch += data
+            });
+            await to(timeout(T_WAIT * 1000));
+            this.msg_err = `Error launching '${path_file}'<<\n\t\t\tReceived: ${error_launch}`;
+            if (error_launch.length) {
+                error_critical = this.msg_err;
+                should.not.exist(error_critical);
+            }
+            error_launch.should.be.equal("");
+        }
+    });
+    it('', async function () {
+        this.name = `4: Looking for '<table>'...`;
+        this.score = 1;
+        if (error_critical) {
+            this.msg_err = error_critical;
+            should.not.exist(error_critical);
+        } else {
+            const expected = "table";
+            [error_nav, resp] = await to(browser.visit(URL));
+            this.msg_ok = `Found '${expected}' in ${path_assignment}`;
+            this.msg_err = `Couldn't find '${expected}' in ${URL}\n\t\t\tError: >>${error_nav}<<\n\t\t\tReceived: >>${browser.html('body')}<<`;
+            browser.assert.elements(expected, {atLeast: 1});
+        }
+    });
+
+    it('', async function () {
+        this.name = `4: Checking 'Edit' implementation...`;
+        this.score = 3;
+        if (error_critical) {
+            this.msg_err = error_critical;
+            should.not.exist(error_critical);
+        } else {
+            const expected = "Question 1";
+            [error_nav, resp] = await to(browser.visit(URL));
+            if (error_nav) {
+                this.msg_err = `Couldn't find '${expected}' in ${URL}\n\t\t\tError: >>${error_nav}<<\n\t\t\tReceived: >>${browser.html('body')}<<`;
+            }
+            [error_nav, resp] = await to(browser.click('a[href="/quizzes/1/edit"]'));
+            if (error_nav) {
+                this.msg_err = `Couldn't find '${expected}' in ${URL}\n\t\t\tError: >>${error_nav}<<\n\t\t\tReceived: >>${browser.html('body')}<<`;
+            }
+            [error_nav, resp] = await to(browser.fill('input[name="question"]', expected));
+            if (error_nav) {
+                this.msg_err = `Couldn't find '${expected}' in ${URL}\n\t\t\tError: >>${error_nav}<<\n\t\t\tReceived: >>${browser.html('body')}<<`;
+            }
+            [error_nav, resp] = await to(browser.fill('input[name="answer"]', "Answer 1"));
+            if (error_nav) {
+                this.msg_err = `Couldn't find '${expected}' in ${URL}\n\t\t\tError: >>${error_nav}<<\n\t\t\tReceived: >>${browser.html('body')}<<`;
+            }
+            try {
+                browser.assert.elements("form", {atLeast: 1});
+                browser.document.forms[0].submit();
+                await to(browser.wait());
+            } catch (e) {
+                error_nav = e;
+            }
+            this.msg_ok = `Found '${expected}' in ${URL}`;
+            this.msg_err = `Couldn't find '${expected}' in ${URL}\n\t\t\tError: >>${error_nav}<<\n\t\t\tReceived: >>${browser.html('body')}<<`;
+            Utils.search(expected, browser.html('body')).should.be.equal(true);
+        }
+    });
+
+    it('', async function () {
+        this.name = `5: Checking 'Delete' implementation...`;
+        this.score = 3;
+        if (error_critical) {
+            this.msg_err = error_critical;
+            should.not.exist(error_critical);
+        } else {
+            const expected = 'a[href="/quizzes/3?_method=DELETE"]';
+            [error_nav, resp] = await to(browser.visit(URL));
+            [error_nav, resp] = await to(browser.click('a[href="/quizzes/3?_method=DELETE"]'));
+            this.msg_ok = `Successfully deleted quiz 3 in ${URL}`;
+            [error_nav, resp] = await to(browser.visit(URL));
+            this.msg_err = `Could not delete quiz 3 clicking '${expected}' in ${URL}\n\t\t\tError: >>${error_nav}`;
+            browser.querySelectorAll(expected).length.should.be.equal(0);
+        }
+    });
+
+    it('', async function () {
+        this.name = `6: Checking 'Play' implementation...`;
+        this.score = 3;
+        if (error_critical) {
+            this.msg_err = error_critical;
+            should.not.exist(error_critical);
+        } else {
+            const expected = /yes/i;
+            [error_nav, resp] = await to(browser.visit(URL));
+            if (error_nav) {
+                this.msg_err = `Couldn't find '${expected}' in ${URL}\n\t\t\tError: >>${error_nav}<<\n\t\t\tReceived: >>${browser.html('body')}<<`;
+            }
+            [error_nav, resp] = await to(browser.click('a[href="/quizzes/1/play"]'));
+            if (error_nav) {
+                this.msg_err = `Couldn't find '${expected}' in ${URL}\n\t\t\tError: >>${error_nav}<<\n\t\t\tReceived: >>${browser.html('body')}<<`;
+            }
+            [error_nav, resp] = await to(browser.fill('input[name=response]', "Answer 1"));
+            if (error_nav) {
+                this.msg_err = `Couldn't find '${expected}' in ${URL}\n\t\t\tError: >>${error_nav}<<\n\t\t\tReceived: >>${browser.html('body')}<<`;
+            }
+            try {
+                browser.assert.elements("form", {atLeast: 1});
+                browser.document.forms[0].submit();
+                await to(browser.wait());
+            } catch (e) {
+                error_nav = e;
+            }
+            this.msg_ok = `Found '${expected}' in ${URL}`;
+            this.msg_err = `Couldn't find '${expected}' in ${URL}\n\t\t\tError: >>${error_nav}<<\n\t\t\tReceived: >>${browser.html('body')}<<`;
+            Utils.search(expected, browser.html('body')).should.be.equal(true);
+        }
+    });
+
+    after("Closing the server", async function () {
+        // kill server
+        if (server) {
+            server.kill();
+            await to(timeout(T_WAIT * 1000));
+        }
+        // restore original db file
+        try {fs.copySync(quizzes_back, quizzes_orig, {"overwrite": true});} catch (e){}
+    });
+});
